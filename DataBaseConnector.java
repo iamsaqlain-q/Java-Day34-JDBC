ef5d8502a8a6b3c703b42f7f646221873113484c
Saqlain <iamkhatik786@gmail.com>
2022-10-14 15:45:10 +0530

    Connect to database

---
+++ DataBaseConnector.java
@@ -0,0 +1,47 @@
+package com.bl.employee.payroll.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DataBaseConnector {
+
+	public static void main(String[] args) {
+		// defined jdbc url, username, password
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "mysql";
+		// establish connection
+		Connection con = null;
+
+		// Driver is loaded or not
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // loading the Driver class
+			System.out.println("Driver loaded.");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("can not find the driver in classpath ..", e);
+		}
+
+		// list all the drivers
+		listDrivers();
+
+		try {
+			System.out.println("Connecting to database:" + jdbcURL);
+			con = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection established successfully...");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// what are the drivers reg in classpath
+	private static void listDrivers() {
+		Enumeration<Driver> listDrivers = DriverManager.getDrivers();
+
+		while (listDrivers.hasMoreElements()) {
+			Driver driverClass = listDrivers.nextElement();
+			System.out.println("" + driverClass.getClass().getName());
+		}
+	}
+}
